8c8a8edf30e905e08e91daf8445900b71b9b0b62
Mike L <mluck1994@gmail.com>
2024-02-08 00:39:00 -0500

    Re-license under MIT license going forward

--- launcher.py
+++ launcher.py
@@ -1,23 +1,5 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2017-2018, 2021-2023 Michael Luck
-# Distributed under the GNU GPL v3. For full terms see the file LICENSE.txt
-
-# This file is part of Simple TTR Launcher.
-
-# Simple TTR Launcher is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# Simple TTR Launcher is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with Simple TTR Launcher. If not, see <http://www.gnu.org/licenses/>.
-
 """
 Handles the main launcher functions including:
 - Displaying the Main Menu
